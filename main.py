7cc4e154df9d6f23bac5b4ee065d0780b853932b
Michal <michalzajdel2002@gmail.com>
2025-06-16 17:43:48 +0200

    all variants

--- main.py
+++ main.py
@@ -15,7 +15,32 @@ features = [
 freq = {}
 for feature in features:
     freq[feature] = df[feature].sum()
-    
+
+
+pairs = list(itertools.combinations(features, 2))
+
+pair_freq = {}
+for f1, f2 in pairs:
+    colname = f"{f1}__{f2}"
+    df[colname] = df[f1] * df[f2]
+    pair_freq[colname] = df[colname].sum()
+
+
+X = df[features]
+y = df['cenaPLN']
+
+model = LinearRegression()
+model.fit(X, y)
+
+results = pd.DataFrame({
+    'udogodnienie': X.columns,
+    'wplyw_na_cene': model.coef_,
+    'liczba_wystapien': [freq[col] for col in X.columns]
+}).sort_values(by='wplyw_na_cene', ascending=False)
+
+print("Wpływ udogodnień na cenę:")
+print(results)
+
 X = df[features]
 y = df['cena_za_m2']
 
@@ -28,16 +53,23 @@ results = pd.DataFrame({
     'liczba_wystapien': [freq[col] for col in X.columns]
 }).sort_values(by='wplyw_na_cene_m2', ascending=False)
 
-print("Wpływ cech i ich kombinacji na cenę za m² (PLN):")
+print("Wpływ udogodnień na cenę za m²:")
 print(results)
 
-pairs = list(itertools.combinations(features, 2))
+X = df[[f"{f1}__{f2}" for f1, f2 in pairs]]
+y = df['cenaPLN']
 
-pair_freq = {}
-for f1, f2 in pairs:
-    colname = f"{f1}__{f2}"
-    df[colname] = df[f1] * df[f2]
-    pair_freq[colname] = df[colname].sum()
+model = LinearRegression()
+model.fit(X, y)
+
+results = pd.DataFrame({
+    'para': X.columns,
+    'wplyw_na_cene': model.coef_,
+    'liczba_wystapien': [pair_freq[col] for col in X.columns]
+}).sort_values(by='wplyw_na_cene', ascending=False)
+
+print("Wpływ kombinacji udogodnień na cenę):")
+print(results)
 
 X = df[[f"{f1}__{f2}" for f1, f2 in pairs]]
 y = df['cena_za_m2']
@@ -51,4 +83,5 @@ results = pd.DataFrame({
     'liczba_wystapien': [pair_freq[col] for col in X.columns]
 }).sort_values(by='wplyw_na_cene_m2', ascending=False)
 
+print("Wpływ kombinacji udogodnień na cenę za m²:")
 print(results)
