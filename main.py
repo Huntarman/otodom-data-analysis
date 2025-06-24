c96205a75c795af972a682d395dae91691e9faac
Michal <michalzajdel2002@gmail.com>
2025-06-24 14:31:55 +0200

    districts feature prices

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ results = pd.DataFrame({
 }).sort_values(by='wplyw_na_cene', ascending=False)
 
 print("Wpływ udogodnień na cenę:")
-print(results)
+results
 
 X = df[features]
 y = df['cena_za_m2']
@@ -54,7 +54,7 @@ results = pd.DataFrame({
 }).sort_values(by='wplyw_na_cene_m2', ascending=False)
 
 print("Wpływ udogodnień na cenę za m²:")
-print(results)
+results
 
 X = df[[f"{f1}__{f2}" for f1, f2 in pairs]]
 y = df['cenaPLN']
@@ -69,7 +69,7 @@ results = pd.DataFrame({
 }).sort_values(by='wplyw_na_cene', ascending=False)
 
 print("Wpływ kombinacji udogodnień na cenę):")
-print(results)
+results
 
 X = df[[f"{f1}__{f2}" for f1, f2 in pairs]]
 y = df['cena_za_m2']
@@ -84,4 +84,49 @@ results = pd.DataFrame({
 }).sort_values(by='wplyw_na_cene_m2', ascending=False)
 
 print("Wpływ kombinacji udogodnień na cenę za m²:")
+results
+
+
+dzielnice_encoded = pd.get_dummies(df['dzielnica'])
+
+interaction_cols = []
+for feature in features:
+    new_cols = []
+    for dzielnica in df['dzielnica'].unique():
+        col_name = f"{dzielnica}_x_{feature}"
+        mask = (df['dzielnica'] == dzielnica)
+        df[col_name] = df[feature] * mask.astype(int)
+        if df[col_name].sum() > 50:
+            interaction_cols.append(col_name)
+        else:
+            df.drop(columns=[col_name], inplace=True)
+
+X = df[interaction_cols]
+y = df['cenaPLN']
+
+model = LinearRegression()
+model.fit(X, y)
+
+results = pd.DataFrame({
+    'interakcja': X.columns,
+    'wplyw_na_cene': model.coef_,
+    'liczba_wystapien': [df[col].sum() for col in X.columns]
+}).sort_values(by='wplyw_na_cene', ascending=False)
+
+print("Wpływ interakcji udogodnień i dzielnic na cenę:")
+print(results)
+
+X = df[interaction_cols]
+y = df['cena_za_m2']
+
+model = LinearRegression()
+model.fit(X, y)
+
+results = pd.DataFrame({
+    'interakcja': X.columns,
+    'wplyw_na_cene': model.coef_,
+    'liczba_wystapien': [df[col].sum() for col in X.columns]
+}).sort_values(by='wplyw_na_cene', ascending=False)
+
+print("Wpływ interakcji udogodnień i dzielnic na cenę za m2:")
 print(results)
